debb0e421fd44eb7f0f8103d1e7796a18103bb6c
MassimoV <massimo.variolo@rockwellautomation.com>
2024-10-09 12:51:19 +0200

    fix: refresh ui on recipe deletion

---
+++ ProjectFiles/NetSolution/RecipeController1.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.SQLiteStore;
+using FTOptix.HMIProject;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.Store;
+using FTOptix.Recipe;
+using FTOptix.OPCUAServer;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+using FTOptix.Core;
+#endregion
+
+public class RecipeController1 : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+}
---
+++ ProjectFiles/NetSolution/RecipesEditorUISetup1.cs
@@ -0,0 +1,25 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.SQLiteStore;
+using FTOptix.HMIProject;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.Store;
+using FTOptix.Recipe;
+using FTOptix.OPCUAServer;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+using FTOptix.Core;
+#endregion
+
+public class RecipesEditorUISetup1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
